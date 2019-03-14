d831232c54f7107ea6155251379d22e6486cd457
keenanwoodall <keenanwoodall@gmail.com>
2019-03-14 17:01:32 -0500

    Created barebones editor code generation

---
+++ Code/Editor/EditorGenerator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Reflection;
+using System.IO;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+using UnityEditor;
+
+namespace Beans.Unity.Editor.EditorGenerator
+{
+	public class EditorGenerator
+	{
+		private MonoScript script;
+		private CodeCompileUnit unitCode;
+
+		private static string Path = "Assets";
+
+		public EditorGenerator (MonoScript script)
+		{
+			this.script = script ?? throw new NullReferenceException ("Script cannot be null.");
+
+			unitCode = new CodeCompileUnit ();
+
+			var namespaceCode = CreateNamespaceCode (script);
+			var classCode = CreateClassCode (script);
+
+			namespaceCode.Types.Add (classCode);
+
+			unitCode.Namespaces.Add (namespaceCode);
+		}
+
+		private CodeNamespace CreateNamespaceCode (MonoScript script)
+		{
+			var namespaceCode = new CodeNamespace ();
+
+			// Set name
+			var name = script.GetClass ().Namespace;
+			if (!string.IsNullOrEmpty (name))
+				namespaceCode.Name = $"{name}Editor";
+
+			// Add imports
+			namespaceCode.Imports.Add (new CodeNamespaceImport ("UnityEditor"));
+
+			return namespaceCode;
+		}
+
+		private CodeTypeDeclaration CreateClassCode (MonoScript script)
+		{
+			var classCode = new CodeTypeDeclaration ($"{script.name}Editor");
+
+			// Make class inherit from Editor
+			classCode.BaseTypes.Add ("Editor");
+			// Make unit a public class
+			classCode.IsClass = true;
+			classCode.TypeAttributes = TypeAttributes.Public;
+
+			return classCode;
+		}
+
+		public MonoScript Generate ()
+		{
+			Path = EditorUtility.SaveFilePanelInProject ("Save script", $"{script.name}Editor", "cs", "", Path);
+
+			if (string.IsNullOrEmpty (Path))
+				return null;
+
+			var provider = CodeDomProvider.CreateProvider ("CSharp");
+			var options = new CodeGeneratorOptions ()
+			{
+				BracingStyle = "C"
+			};
+
+			using (var writer = new StreamWriter (Path))
+				provider.GenerateCodeFromCompileUnit (unitCode, writer, options);
+
+			return (MonoScript)AssetDatabase.LoadAssetAtPath (Path, typeof (MonoScript));
+		}
+	}
+}
---
+++ Code/Editor/EditorGeneratorWindow.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Beans.Unity.Editor.EditorGenerator
+{
+	public class EditorGeneratorWindow : EditorWindow
+	{
+		private class Content
+		{
+			public static readonly GUIContent Title = new GUIContent ("Editor Generator");
+			public static readonly GUIContent Script = new GUIContent ("Script");
+		}
+
+		private class Styles
+		{
+			public static readonly Vector2 MinSize = new Vector2 (300, 200);
+
+			public static readonly GUIStyle Title;
+			public static readonly GUIStyle FilePath;
+
+			static Styles ()
+			{
+				Title = new GUIStyle (EditorStyles.largeLabel)
+				{
+					alignment = TextAnchor.MiddleCenter,
+					fontStyle = FontStyle.Bold
+				};
+
+				FilePath = new GUIStyle (EditorStyles.label)
+				{
+					alignment = TextAnchor.MiddleRight
+				};
+			}
+		}
+
+		private MonoScript script;
+
+		private EditorGenerator generator;
+
+		[MenuItem ("Tools/Editor Generator")]
+		[MenuItem ("Window/Editor Generator")]
+		public static void GetWindow ()
+		{
+			var window = GetWindow<EditorGeneratorWindow> ();
+
+			window.titleContent = Content.Title;
+			window.minSize = Styles.MinSize;
+		}
+
+		private void OnGUI ()
+		{
+			EditorGUILayout.LabelField (Content.Title, Styles.Title);
+
+			EditorGUILayout.Space ();
+
+			script = (MonoScript)EditorGUILayout.ObjectField (Content.Script, script, typeof (MonoScript), false);
+
+			using (new EditorGUI.DisabledGroupScope (script == null))
+			{
+				if (GUILayout.Button ("Generate"))
+				{
+					generator = new EditorGenerator (script);
+					generator.Generate ();
+					AssetDatabase.Refresh ();
+				}
+			}
+		}
+
+		private string OpenSelectFilePanel ()
+		{
+			return EditorUtility.OpenFilePanel ("Select script", "Assets", "cs");
+		}
+	}
+}
